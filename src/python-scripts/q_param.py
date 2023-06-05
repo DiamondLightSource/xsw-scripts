78eb235490dd2babfac621298f1c015aedf39190
tizayi <57833318+tizayi@users.noreply.github.com>
2023-06-05 09:39:42 +0100

    add pyproject toml

--- src/python-scripts/q_param.py
+++ src/python-scripts/q_param.py
@@ -16,10 +16,26 @@ azimuthal_2_spin_dict: Dict[str, Union[str, List[float]]] = {
 
 
 def get_orbital(azimuthal_qn: int) -> Optional[str]:
+    """Translates a intiger azimuthal quantum number to the correposing letter orbital.
+        Args:
+            azimuthal_qn: the azimuthal quntim number
+        
+        Returns:
+            Optional[str]: the letter of that orbital
+    """
     return list(azimuthal_2_spin_dict.keys())[azimuthal_qn]
 
 
 def get_spin_as_string(azimuthal_qn: int, spin_qn: float) -> Optional[str]:
+    """Translates a spin quanqtum number float into a string represing the coresponding fraction.
+    
+    Args:
+        azimuthal_qn: the azimuthal quantum number
+        spin_qn: the spin quantum number
+
+    Returns:
+        Optional[str]: string represing the fraction of the spin quantum number 
+    """
     orbital = get_orbital(azimuthal_qn)
     spins = azimuthal_2_spin_dict[orbital]
 
@@ -32,36 +48,36 @@ def get_spin_as_string(azimuthal_qn: int, spin_qn: float) -> Optional[str]:
 
 
 def q_param(
-    data_dir: Path,
+    data_file: Path,
     Z: int,
     principal_qn: int,
     azimuthal_qn: int,
     spin_qn: float,
     plotter: Optional[int] = 1,
-) -> Optional[Tuple[List]]:
+) -> Optional[Tuple[List[numpy.ndarray]]]:
     """Gets q_params from the q_params .txt"""
+    if plotter:
+        print(
+        f"{constants.Z[Z]} {principal_qn}{get_orbital(azimuthal_qn)}{get_spin_as_string(azimuthal_qn,spin_qn)}"
+        )
 
-    with open(data_dir / Path("q_param.txt"), "r") as param_file:
-        if plotter:
-            print(
-                f"{constants.Z[Z]} {principal_qn}{get_orbital(azimuthal_qn)}{get_spin_as_string(azimuthal_qn,spin_qn)}"
-            )
-
+    with open(data_file, "r") as param_file:
         name = f"{Z} {principal_qn}{get_orbital(azimuthal_qn)}{get_spin_as_string(azimuthal_qn,spin_qn)}"
-        print(name)
         for line in param_file:
             if line.strip() == name:
-                print(line.strip())
                 Eb = param_file.readline()
                 Erow = numpy.fromstring(param_file.readline(), int, sep=" ")
+                
                 sigma = [Erow, numpy.fromstring(param_file.readline(), float, sep=" ")]
                 beta = [Erow, numpy.fromstring(param_file.readline(), float, sep=" ")]
                 gamma = [Erow, numpy.fromstring(param_file.readline(), float, sep=" ")]
                 delta = [Erow, numpy.fromstring(param_file.readline(), float, sep=" ")]
-                print(f"beta:{beta} gamma:{gamma} delta:{delta} Eb:{Eb}")
+                
+                print(f"beta: {beta} \ngamma: {gamma} \ndelta: {delta} \nEb: {Eb}")
                 return (beta, gamma, delta, Eb)
         raise NotImplementedError(f"{name} not found in params.txt")
+        
 
 
 if __name__ == "__main__":
-    q_param(Path("/scratch/nal89286/xsw-script/src/fpfpp/"), 7, 2, 1, 1.5)
+    q_param(Path("../fpfpp/q_param.txt"), 7, 2, 1, 1.5)
