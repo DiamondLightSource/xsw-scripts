b4d3d549636daf40215b4cc1abd1e6e409c3b81c
elliot <elliot.mullane@diamond.ac.uk>
2025-03-18 14:56:13 +0000

    added xps_extension, containing extend_cs to create xsw data

---
+++ src/xsw_scripts/xps_extension.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from xsw_scripts.xps_datagen.NNDataBuilderPeter import *
+
+def extend_cs(cs, num_peaks, modulations, lower_bound = -2, upper_bound = 2, plot = True, num_plots_x = 4, num_plots_y = 5):
+    #cs a 46 parameter array, where the 15th, 21st, 27th, 33rd, 39th, 45th correspond to the intensities of individual peaks
+    if len(modulations) != num_peaks:
+        print('number of modulations does not equal number of peaks.')
+        return
+    
+    curve_samples = []
+    for i in range(len(modulations)):
+        current_sample = []
+        current_curve = modulations[i][0] #result of predict modulation: two arrays, one containing e-e_bragg vs xsw profile, the other e-e_bragg vs reflection
+        for tuple in current_curve:
+            if tuple[0] >= lower_bound and tuple[0] <= upper_bound:
+                current_sample.append(tuple)
+        curve_samples.append(current_sample)
+
+    ##are predict_modulation outputs consistent? will they have the same number of values - naively assume yes for now
+    current_slice = np.copy(cs)
+    for i in range(len(current_sample)):
+        for j in range(num_peaks):
+            current_sample = curve_samples[j]
+            current_slice[15 + 6*j] = cs[15 + 6*j] * current_sample[i][1]
+        if i == 0:
+            stacked_slices = np.copy(current_slice)
+        else:
+            stacked_slices = np.column_stack([stacked_slices, current_slice])
+
+    obtained_spectra = build_spectra(stacked_slices)
+    
+    if plot == True:
+        num_plots = num_plots_x * num_plots_y
+        fig, ax = plt.subplots(num_plots_y, num_plots_x, sharey = True)
+        count = 0
+        interval = len(current_sample)//num_plots
+        for ax in fig.get_axes():
+            ax.plot(obtained_spectra[interval*count][0], obtained_spectra[interval * count][1])
+            ax.xaxis.set_inverted(True)
+            count += 1
+
+        plt.show()
+    
+    
+    return obtained_spectra
